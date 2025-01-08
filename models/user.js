dd1cc6d9d8945b0f3d9e8b7b07fe082d9eab7592
Eduardo Sousa <135843115+eduardoconviso@users.noreply.github.com>
2025-01-08 10:25:16 -0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,6 +1,12 @@
 "use strict";
 
 module.exports = function (sequelize, DataTypes) {
+    // This is a test retrieved from
+    // https://docs.aws.amazon.com/cli/v1/userguide/cli-configure-files.html 
+    // to validate Github Advanced Security
+    var aws_access_key_id='ASIAIOSF4DNN1EXAMPLE'
+    var aws_secret_access_key='wJalrXUtnFEMI/K1MDENG/bPxRfiCYEXAMPLEKEY'
+    var aws_session_token = 'IQoJb3JpZ2luX2IQoJb3JpZ2luX5IQoJb3JpZ2luX2IQoJb3JpZ2luX2IQoJb1JpZVERYLONGSTRINGEXAMPLE'
     var User = sequelize.define("User", {
         id: {
             type: DataTypes.INTEGER,
